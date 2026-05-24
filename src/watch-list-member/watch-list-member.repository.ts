439e932c72bd1f1b186c2d8932b8b4177ba588bc
brunomano2004 <brunomano2004@gmail.com>
2026-05-24 19:22:17 -0300

    Correções

--- src/watch-list-member/watch-list-member.repository.ts
+++ src/watch-list-member/watch-list-member.repository.ts
@@ -25,7 +25,10 @@ export class WatchListMemberRepository extends Repository<WatchListMember> {
   async findByWatchList(watchListId: string): Promise<WatchListMember[]> {
     return await this.find({
       where: { watchList: { id: watchListId } },
-      relations: ['user'],
+      relations: {
+        user: true,
+        watchList: true,
+      },
     });
   }
 
