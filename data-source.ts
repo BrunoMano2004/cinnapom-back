4d78faac053eb236fd021e8c13b2c09be2e14931
BrunoMano2004 <brunomano2004@gmail.com>
2026-06-15 20:48:31 -0300

    Ajustando data-source

--- data-source.ts
+++ data-source.ts
@@ -5,6 +5,6 @@ dotenv.config();
 export default new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  migrations: [__dirname + '/migrations/*{.ts,.js}'],
+  entities: ['src/**/*.entity.ts', 'dist/src/**/*.entity.js'],
+  migrations: ['src/migrations/*.ts', 'dist/src/migrations/*.js'],
 });
